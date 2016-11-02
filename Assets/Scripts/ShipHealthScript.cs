80fa0d14d2038562a4068cf7c019e77163a35ae3
Miłosz <gorgon94@gmail.com>
2016-11-02 01:36:41 +0100

    Dodanie paska zdrowia statku, aktualizacja gitignore

---
+++ Assets/Scripts/ShipHealthScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShipHealthScript : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+        
+    }
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        healthSlider.value = currentHealth;
+    }
+}
