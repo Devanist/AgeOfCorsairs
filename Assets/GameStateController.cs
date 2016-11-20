4c3734c621620fec664103e031e5648216876ed8
Dominik Dziwisz <dziwisz@yahoo.pl>
2016-11-20 20:40:06 +0100

    Pauzowanie gry po wejsciu do miasta lub wcisnieciu buttona od menu, umieszczenie buttona od menu na ekranie gry

---
+++ Assets/GameStateController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Skrypt odpowiadajacy za przechowywanie stanow gry
+ **/
+public class GameStateController : MonoBehaviour {
+
+    /**
+     * Enum zawierajacy stany gry
+     **/
+    public enum GameState {
+        PAUSED,
+        PLAYING
+    }
+
+    public static GameState gameState;
+
+	// Use this for initialization
+	void Start () {
+        gameState = GameState.PLAYING;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PauseMenuController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Pokazywanie / chowanie menu pauzy, pauzowanie gry
+ **/
+public class PauseMenuController : MonoBehaviour {
+
+    GameObject pauseMenu;
+    public GameObject enterCityButton;
+
+    //Do chowania buttona od miasta
+    private bool wasEnterCityButtonActive;
+
+
+    void Start () {
+        pauseMenu = GameObject.FindGameObjectsWithTag("Pause Menu")[0];
+
+        wasEnterCityButtonActive = false;
+        pauseMenu.SetActive(false);
+    }
+	
+
+	void Update () {
+	
+	}
+
+    public void showPauseMenu() {
+        pauseMenu.SetActive(true);
+
+        if(enterCityButton.activeSelf) {
+            wasEnterCityButtonActive = true;
+            enterCityButton.SetActive(false);
+        }
+
+        GameStateController.gameState = GameStateController.GameState.PAUSED;
+    }
+
+    public void hidePauseMenu() {
+        pauseMenu.SetActive(false);
+
+        if(wasEnterCityButtonActive) {
+            wasEnterCityButtonActive = false;
+            enterCityButton.SetActive(true);
+        }
+
+        GameStateController.gameState = GameStateController.GameState.PLAYING;
+    }
+}
