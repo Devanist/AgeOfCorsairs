6158b98747d8dfc61a3bca609a715dacfa700fbe
Devanist <devan93@gmail.com>
2016-11-21 17:26:02 +0100

    working mortar

--- Assets/FortShootingScript.cs
+++ Assets/FortShootingScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using System;
 
 public class FortShootingScript : MonoBehaviour {
 
@@ -8,11 +9,18 @@ public class FortShootingScript : MonoBehaviour {
     private CircleCollider2D _rangeCollider;
     private bool shouldFire;
     private float lastShootTime;
+    private float lastMortarShootTime;
     private GameObject PlayerShip;
+    private Vector3 lastPlayerPosition;
+    private GameObject _mt;
 
+    public GameObject MortarTarget;
     public GameObject Bullet;
+    public float MortarFactor = 180.0f;
     public float FiringSpeed = 5.0f;
+    public float FiringMortarSpeed = 8.0f;
     List<GameObject> _bullets = new List<GameObject>();
+    List<GameObject> _mortarBullets = new List<GameObject>();
 
 // Use this for initialization
 void Start () {
@@ -21,27 +29,83 @@ void Start () {
         PlayerShip = GameObject.Find("Ship");
         shouldFire = false;
         lastShootTime = 0.0f;
+        lastMortarShootTime = 0.0f;
+
+        _mt = Instantiate(MortarTarget);
+        _mt.SetActive(false);
 
         for (int i = 0; i < 3; i++) {
             var bullet = Instantiate(Bullet);
             bullet.SetActive(false);
             _bullets.Add(bullet);
         }
+
+        for(int i = 0; i < 5; i++) {
+            var bullet = Instantiate(Bullet);
+            bullet.SetActive(false);
+            _mortarBullets.Add(bullet);
+        }
+
     }
 	
 	// Update is called once per frame
 	void Update () {
 	    if(shouldFire == true && Time.time - lastShootTime > FiringSpeed) {
-            Debug.Log("firing!");
             lastShootTime = Time.time;
             Fire();
         }
+        if(shouldFire == true && Time.time - lastMortarShootTime > FiringMortarSpeed) {
+            lastMortarShootTime = Time.time;
+            MortarFire();
+        }
+        lastPlayerPosition = new Vector3(PlayerShip.transform.position.x, PlayerShip.transform.position.y, PlayerShip.transform.position.z);
 	}
+
+    //Wystrzał z moździerza, kule spadają z góry ekranu w miejsce, w które gracz może wpłynąć w następnej chwili.
+    private void MortarFire() {
+        Vector3 bulletStartPosition;
+        Vector3 bulletTargetPosition;
+        Vector3 playerPosition = PlayerShip.transform.position;
+
+        float[] shootingOffsetX = { -0.2f, -0.1f, 0, 0.2f, 0.1f };
+        float[] shootingOffsetY = { -0.2f, 0.1f, 0, 0.2f, -0.3f };
+
+        for (int i = 0; i < 5; i++) {
+
+            if (!_mortarBullets[i].activeInHierarchy) {
+                var bulletMoveScript = _mortarBullets[i].GetComponent<MortarBulletMoveScript>();
+                bulletMoveScript.MortarTarget = _mt;
+                bulletStartPosition = new Vector3(playerPosition.x - 3.0f, playerPosition.y - 8.0f, -10.1f);
+
+                bulletTargetPosition = new Vector3(
+                    playerPosition.x + (playerPosition.x - lastPlayerPosition.x) * MortarFactor + shootingOffsetX[i], 
+                    playerPosition.y + (playerPosition.y - lastPlayerPosition.y) * MortarFactor + shootingOffsetY[i], 
+                    playerPosition.z
+                );
+
+                Debug.Log((playerPosition.x - lastPlayerPosition.x) * MortarFactor);
+                Debug.Log((playerPosition.y - lastPlayerPosition.y) * MortarFactor);
+
+                if(i == 2) {
+                    bulletMoveScript.isCentral = true;
+                };
+
+                _mt.SetActive(true);
+                _mt.transform.position = bulletTargetPosition;                
+
+                _mortarBullets[i].transform.position = bulletStartPosition;
+                bulletMoveScript.TargetPosition = bulletTargetPosition;
+
+                _mortarBullets[i].SetActive(true);
+            }
+
+        }
+
+    }
 
     void OnTriggerEnter2D(Collider2D collider) {
         if(this.tag == "Enemy Island") {
             shouldFire = true;
-            Debug.Log("trigger");
         }
     }
 
@@ -49,22 +113,27 @@ void Start () {
         shouldFire = false;
     }
 
+    //Wystrzał z dział
     private void Fire() {
 
         Vector3 bulletStartPosition;
-        Vector3 bulletTargetPosition;        
+        Vector3 bulletTargetPosition;
 
         Vector3 playerPosition = PlayerShip.transform.position;
+
+        float[] shootingOffset = { -0.2f, 0, 0.2f };
 
         for(int i = 0; i < 3; i++) {
 
             if (!_bullets[i].activeInHierarchy) {
-                var bulletMoveScript = _bullets[i].GetComponent<BulletMoveScript>();
+                var bulletMoveScript = _bullets[i].GetComponent<MortarBulletMoveScript>();
 
-                bulletStartPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                bulletStartPosition = new Vector3(transform.position.x + shootingOffset[i], transform.position.y + shootingOffset[i], transform.position.z);
 
                 //Osobna referencja na wypadek gdybyśmy chcieli strzelać przed gracza
                 bulletTargetPosition = playerPosition;
+                bulletTargetPosition.x += shootingOffset[i];
+                bulletTargetPosition.y += shootingOffset[i];
 
                 _bullets[i].transform.position = bulletStartPosition;
 
---
+++ Assets/Scripts/MortarBulletMoveScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MortarBulletMoveScript : MonoBehaviour {
+
+    public Vector3 TargetPosition = new Vector3();
+    public GameObject MortarTarget;
+    public float Speed = 10f;
+    public bool isCentral;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (gameObject.activeInHierarchy) {
+            float step = Speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, TargetPosition, step);
+
+            if (transform.position == TargetPosition) {
+                gameObject.SetActive(false);
+                if (isCentral) {
+                    MortarTarget.SetActive(false);
+                }
+            }
+        }
+
+    }
+
+}
