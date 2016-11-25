0c6030b2e2bd9d2f6ec6d8c781559e8d3b1a5ddf
Devanist <devanist@outlook.com>
2016-11-25 17:29:53 +0100

    added repair panel to shipyard

--- Assets/ShipyardController.cs
+++ Assets/ShipyardController.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class ShipyardController : MonoBehaviour {
 
-    GameObject  ShipsButton, 
+    GameObject ShipsButton,
                 UpgradesButton,
+                RepairsButton,
                 ShipsPanel,
-                UpgradesPanel;
+                UpgradesPanel,
+                RepairsPanel;
 
 	// Use this for initialization
 	void Start () {
         ShipsButton = GameObject.Find("ShipsButton");
         UpgradesButton = GameObject.Find("UpgradesButton");
         ShipsPanel = GameObject.Find("ShipsPanel");
+        RepairsButton = GameObject.Find("RepairsButton");
+        RepairsPanel = GameObject.Find("RepairsPanel");
         UpgradesPanel = GameObject.Find("UpgradesPanel");
 
         ShipsPanel.SetActive(false);
+        UpgradesPanel.SetActive(false);
     }
 	
 	// Update is called once per frame
@@ -25,11 +30,19 @@ public class ShipyardController : MonoBehaviour {
 
     public void onShipsClick() {
         UpgradesPanel.SetActive(false);
+        RepairsPanel.SetActive(false);
         ShipsPanel.SetActive(true);
     }
 
     public void onUpgradesClick() {
         UpgradesPanel.SetActive(true);
         ShipsPanel.SetActive(false);
+        RepairsPanel.SetActive(false);
+    }
+
+    public void onRepairsClick() {
+        UpgradesPanel.SetActive(false);
+        ShipsPanel.SetActive(false);
+        RepairsPanel.SetActive(true);
     }
 }
