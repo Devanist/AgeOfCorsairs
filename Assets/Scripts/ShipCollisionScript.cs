87d20687abdc70316f7a1272bc5b918b621d328c
Quisy <lamprecht.mariusz@gmail.com>
2016-11-28 23:21:26 +0100

    Added simple collide detection with mortar bullets

---
+++ Assets/Scripts/ShipCollisionScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Enums;
+
+public class ShipCollisionScript : MonoBehaviour
+{
+    public GameObject ShipLeftSideShootingArrow;
+    public GameObject ShipRightSideShootingArrow;
+
+
+    private ShipHealthScript _shipHealthScript;
+
+    // Use this for initialization
+    void Start()
+    {
+        _shipHealthScript = GetComponent<ShipHealthScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.tag == "Mortar Bullet")
+        {
+            if (collider.transform.position.x < transform.position.x
+                && transform.rotation.z >= -0.90
+                && transform.rotation.z <= 0.90)
+            {
+                _shipHealthScript.AddHealth(-5,ShipSide.Left);
+
+                if(_shipHealthScript.LeftSideHealth==0)
+                    ShipLeftSideShootingArrow.SetActive(false);
+            }
+            else if (collider.transform.position.x < transform.position.x
+               && (transform.rotation.z < -0.90 || transform.rotation.z > 0.90))
+            {
+                _shipHealthScript.AddHealth(-5, ShipSide.Right);
+
+                if (_shipHealthScript.RightSideHealth == 0)
+                    ShipRightSideShootingArrow.SetActive(false);
+            }
+        }
+    }
+
+
+}
--- Assets/Scripts/ShipHealthScript.cs
+++ Assets/Scripts/ShipHealthScript.cs
@@ -1,31 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Enums;
 using UnityEngine.UI;
 
 public class ShipHealthScript : MonoBehaviour
 {
+    public const int MaxSideHealth = 50;
+
     public int maxHealth = 100;
     public int startingHealth = 100;
     public int currentHealth;
     public Slider healthSlider;
 
     private double oldHealthProcent;
+    public int LeftSideHealth = MaxSideHealth;
+    public int RightSideHealth = MaxSideHealth;
+
+
     void Awake()
     {
         healthSlider.maxValue = maxHealth;
         currentHealth = startingHealth;    
     }
 
-    void Start ()
+    void Start()
     {
-	
-	}
-	
-	void Update ()
+
+    }
+
+    void Update()
     {
         healthSlider.maxValue = maxHealth;
         healthSlider.value = currentHealth;
         double healthProcent = ((double)currentHealth / (double)maxHealth) * 100;
         SoundManager.instance.BurnSound(healthProcent);
+    }
+
+
+    public void AddHealth(int health, ShipSide shipSide)
+    {
+        switch (shipSide)
+        {
+            case ShipSide.Left:
+                LeftSideHealth += health;
+                if (LeftSideHealth < 0)
+                    LeftSideHealth = 0;
+
+                if (LeftSideHealth > MaxSideHealth)
+                    LeftSideHealth = MaxSideHealth;
+                break;
+            case ShipSide.Right:
+                RightSideHealth += health;
+                if (RightSideHealth < 0)
+                    RightSideHealth = 0;
+
+                if (RightSideHealth > MaxSideHealth)
+                    RightSideHealth = MaxSideHealth;
+                break;
+        }
+
+        currentHealth += health;
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        if (currentHealth > 100)
+            currentHealth = 100;
+
     }
 }
