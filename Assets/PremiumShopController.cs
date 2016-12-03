0d16a2c960b7167bf6b3f847473b15b2e2203039
Devanist <devanist@outlook.com>
2016-12-03 20:15:03 +0100

    added premium shop mockup

---
+++ Assets/PremiumShopController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PremiumShopController : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("PREMIUMSHOP").SetActive(false);    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
